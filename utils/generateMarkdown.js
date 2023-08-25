014fd6515132b3924298545e3b2643a82b211381
Maya McCann <mccannmaya@gmail.com>
2023-08-25 15:26:54 -0400

    generateMarkdown.js for licenses

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -22,11 +22,18 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license !== "none") {
-  return 'This project has a license of ${renderLicenseBadge(license)}', ${renderLicenseLink(license)}'
-} else {
-  return "";
-}
+  if (license !== 'no license') {
+  return '
+    ## [License](#table-of-contents)
+
+  This application has this license:
+
+  ${renderLicenseLink(license)}
+  ';
+  } else {
+    return '';
+  }
+
 }
 
 // TODO: Create a function to generate markdown for README
