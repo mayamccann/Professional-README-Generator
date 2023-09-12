3e07c5aef1fa286345fbab212827498a53d04559
Maya McCann <mccannmaya@gmail.com>
2023-09-12 19:57:28 -0400

    Delete code in generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,46 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  if (license !== 'no lincense') {
-    return 
-![badge](https://img.sheilds.io/badge/license-${license}-blue)
-;
-}else{
-  return '';
-}
-}
-
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
-  switch (license) {
-    case "MIT":
-      return "[MIT Link](https://www.opensource.org/license/MIT)"
-  }
-}
-
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (license !== 'no license') {
-  return '
-    ## [License](#table-of-contents)
-
-  This application has this license:
-
-  ${renderLicenseLink(license)}
-  ';
-  } else {
-    return '';
-  }
-
-}
-
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
-}
-
-module.exports = generateMarkdown;
