fe77c85154f791949ee9b6379503217c1fabf1b3
Maya McCann <mccannmaya@gmail.com>
2023-09-12 19:25:05 -0400

    GitHub username const questions if statement

--- utils/index.js
+++ utils/index.js
@@ -5,6 +5,25 @@ const inquirer = require('inquirer');
 //Linking the pg --> README
 const generatePage = require('./utils/generateMarkdown.js');
 
-//Array (questions for user)
+//Array (questions for user), inquirer to prompt questions for users
 const questions = () => {
+    return inquirer.createPromptModule([
+    {
+
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+        validate: nameInput => {
+            if (nameInput) => {
+                return true;
+            } else {
+                console.log('Enter GitHub username.');
+                return false;
+            }
+        }
+    },
     
+
+        
+
+
