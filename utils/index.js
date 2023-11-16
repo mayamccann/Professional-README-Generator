ab0fa5048575e6ffd1636979169427dc3252ebf1
Maya McCann <mccannmaya@gmail.com>
2023-11-15 20:54:05 -0500

    const question for title

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,18 @@
 // TODO: Include packages needed for this application
+
 const generateMarkdown = require("./utils/generateMarkdown");
+
 // TODO: Create an array of questions for user input
-const questions = [];
+
+const questions = [
+
+{ 
+    type: "input",
+    message: "For your application, what is the title?",
+    name: "title",
+}
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
